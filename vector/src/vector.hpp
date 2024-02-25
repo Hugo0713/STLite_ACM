09b2eb09b4f20b139acd251f78639278d676d968
Hugo0713 <hugo0713@sjtu.edu.cn>
2024-02-25 22:16:10 +0800

    feat:class const_iterator and all functions of class vector

--- vector/src/vector.hpp
+++ vector/src/vector.hpp
@@ -11,16 +11,17 @@
 #include <cmath>
 #include <string>
 
+// 参考资料：stl源码解析
 namespace sjtu
 {
 	template <typename T>
 	class vector
 	{
 	private:
-		T *data;
-		size_t size_;
-		size_t capacity;
-		std::allocator<T> alloc;
+		T *data;				 // 存储数据
+		size_t size_;			 // 已占容量
+		size_t capacity;		 // 总容量
+		std::allocator<T> alloc; // 空间配置器
 
 	public:
 		/**
@@ -105,8 +106,8 @@ namespace sjtu
 				pos -= n;
 				return *this;
 			}
-			
-			iterator operator++(int) 
+
+			iterator operator++(int)
 			{
 				if (pos + 1 >= vec->size_)
 				{
@@ -114,8 +115,8 @@ namespace sjtu
 				}
 				return iterator(pos + 1, vec);
 			}
-			
-			iterator &operator++() 
+
+			iterator &operator++()
 			{
 				if (pos + 1 >= vec->size_)
 				{
@@ -124,8 +125,8 @@ namespace sjtu
 				++pos;
 				return *this;
 			}
-			
-			iterator operator--(int) 
+
+			iterator operator--(int)
 			{
 				if (pos < 1)
 				{
@@ -133,8 +134,8 @@ namespace sjtu
 				}
 				return iterator(pos - 1, vec);
 			}
-			
-			iterator &operator--() 
+
+			iterator &operator--()
 			{
 				if (pos < 1)
 				{
@@ -143,26 +144,26 @@ namespace sjtu
 				--pos;
 				return *this;
 			}
-			
-			T &operator*() const 
+
+			T &operator*() const
 			{
 				return vec->data[pos];
 			}
-			
-			bool operator==(const iterator &rhs) const 
+
+			bool operator==(const iterator &rhs) const
 			{
 				return (pos == rhs.pos) && (vec == rhs.vec);
 			}
-			bool operator==(const const_iterator &rhs) const 
+			bool operator==(const const_iterator &rhs) const
 			{
 				return (pos == rhs.pos) && (vec == rhs.vec);
 			}
-			
-			bool operator!=(const iterator &rhs) const 
+
+			bool operator!=(const iterator &rhs) const
 			{
 				return !(*this == rhs);
 			}
-			bool operator!=(const const_iterator &rhs) const 
+			bool operator!=(const const_iterator &rhs) const
 			{
 				return !(*this == rhs);
 			}
@@ -180,6 +181,118 @@ namespace sjtu
 		private:
 			size_t pos;
 			const vector *vec;
+
+		public:
+			const_iterator() : pos(0), vec(nullptr) {}
+			const_iterator(size_t position, vector *v) : pos(position), vec(v) {}
+			const_iterator(const const_iterator &other) : pos(other.pos), vec(other.vec) {}
+
+			const_iterator operator+(const int &n) const
+			{
+				if (pos + n >= vec->size_)
+				{
+					throw index_out_of_bound();
+				}
+				return const_iterator(pos + n, vec);
+			}
+			const_iterator operator-(const int &n) const
+			{
+				if (pos - n < 0)
+				{
+					throw index_out_of_bound();
+				}
+				return const_iterator(pos - n, vec);
+			}
+
+			int operator-(const const_iterator &rhs) const
+			{
+				if (vec != rhs.vec)
+				{
+					throw invalid_iterator();
+				}
+				return pos - rhs.pos;
+			}
+			const_iterator &operator+=(const int &n)
+			{
+				// return (*this + n);
+				if (pos + n >= vec->size_)
+				{
+					throw index_out_of_bound();
+				}
+				pos += n;
+				return *this;
+			}
+			const_iterator &operator-=(const int &n)
+			{
+				// return (*this - n);
+				if (pos - n < 0)
+				{
+					throw index_out_of_bound();
+				}
+				pos -= n;
+				return *this;
+			}
+
+			const_iterator operator++(int)
+			{
+				if (pos + 1 >= vec->size_)
+				{
+					throw index_out_of_bound();
+				}
+				return const_iterator(pos + 1, vec);
+			}
+
+			const_iterator &operator++()
+			{
+				if (pos + 1 >= vec->size_)
+				{
+					throw index_out_of_bound();
+				}
+				++pos;
+				return *this;
+			}
+
+			const_iterator operator--(int)
+			{
+				if (pos < 1)
+				{
+					throw index_out_of_bound();
+				}
+				return const_iterator(pos - 1, vec);
+			}
+
+			const_iterator &operator--()
+			{
+				if (pos < 1)
+				{
+					throw index_out_of_bound();
+				}
+				--pos;
+				return *this;
+			}
+
+			T &operator*() const
+			{
+				return vec->data[pos];
+			}
+
+			bool operator==(const iterator &rhs) const
+			{
+				return (pos == rhs.pos) && (vec == rhs.vec);
+			}
+			bool operator==(const const_iterator &rhs) const
+			{
+				return (pos == rhs.pos) && (vec == rhs.vec);
+			}
+
+			bool operator!=(const iterator &rhs) const
+			{
+				return !(*this == rhs);
+			}
+			bool operator!=(const const_iterator &rhs) const
+			{
+				return !(*this == rhs);
+			}
 		};
 
 		vector() : data(nullptr), size_(0), capacity(0) {}
@@ -295,23 +408,31 @@ namespace sjtu
 			}
 			return data[size_ - 1];
 		}
-		/**
-		 * returns an iterator to the beginning.
-		 */
-		iterator begin() {}
-		const_iterator cbegin() const {}
-		/**
-		 * returns an iterator to the end.
-		 */
-		iterator end() {}
-		const_iterator cend() const {}
+
+		iterator begin()
+		{
+			return iterator(0, this);
+		}
+		const_iterator cbegin() const
+		{
+			return const_iterator(0, this);
+		}
+
+		iterator end()
+		{
+			return iterator(size_ - 1, this);
+		}
+		const_iterator cend() const
+		{
+			return const_iterator(size_ - 1, this);
+		}
 
 		bool empty() const
 		{
 			return (size_ == 0);
 		}
 
-		size_t size_() const
+		size_t size() const
 		{
 			return size_;
 		}
@@ -331,26 +452,64 @@ namespace sjtu
 		 * inserts value before pos
 		 * returns an iterator pointing to the inserted value.
 		 */
-		iterator insert(iterator pos, const T &value) {}
+		iterator insert(iterator pos, const T &value)
+		{
+			size_t ind = pos - begin();
+			return insert(ind, value);
+		}
 		/**
 		 * inserts value at index ind.
 		 * after inserting, this->at(ind) == value
 		 * returns an iterator pointing to the inserted value.
 		 * throw index_out_of_bound if ind > size (in this situation ind can be size because after inserting the size will increase 1.)
 		 */
-		iterator insert(const size_t &ind, const T &value) {}
+		iterator insert(const size_t &ind, const T &value)
+		{
+			if (ind < 0 || ind > size_)
+			{
+				throw index_out_of_bound();
+			}
+			if (size_ >= capacity)
+			{
+				Double();
+			}
+			const size_t elem_after = size_ - ind;
+			iterator pos_old(ind, this);
+			iterator pos_new(ind + 1, this);
+			uninitialized_copy_n(pos_old, size_ - ind + 1, pos_new);
+			alloc.construct(data + ind, value);
+			++size_;
+			return begin() + ind;
+
+		}
 		/**
 		 * removes the element at pos.
 		 * return an iterator pointing to the following element.
 		 * If the iterator pos refers the last element, the end() iterator is returned.
 		 */
-		iterator erase(iterator pos) {}
+		iterator erase(iterator pos) 
+		{
+			size_t ind = pos - begin();
+			return erase(ind);
+		}
 		/**
 		 * removes the element with index ind.
 		 * return an iterator pointing to the following element.
 		 * throw index_out_of_bound if ind >= size
 		 */
-		iterator erase(const size_t &ind) {}
+		iterator erase(const size_t &ind) 
+		{
+			if (ind < 0 || ind >= size_)
+			{
+				throw index_out_of_bound();
+			}
+			iterator pos_old(ind + 1, this);
+			iterator pos_new(ind, this);
+			uninitialized_copy_n(pos_old, size_ - ind, pos_new);
+			alloc.destroy(data + ind);
+			--size_;
+			return begin() + ind;
+		}
 
 		void push_back(const T &value)
 		{
